0eae451e13e775e0a100b71215dc70bb524cc178
Diana Agapkina <agapkinadiana@users.noreply.github.com>
2020-09-28 20:55:26 +0300

    2 done

---
+++ PWS_2/Lab2/Controllers/ValuesController.cs
@@ -0,0 +1,38 @@
+﻿using Lab2.Models;
+using System.Web;
+using System.Web.Http;
+
+namespace Lab2.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        public string Get()
+        {
+            return StackService.Get();
+        }
+
+        public string Post([FromUri]int result)
+        {
+            return StackService.Post(result);
+        }
+
+        public string Put([FromUri]int add)
+        {
+            return StackService.Put(add);
+        }
+
+        public string Delete()
+        {
+            string response;
+            if (StackService.StackState() > 0)
+            {
+                response = StackService.Delete();
+            }
+            else
+            {
+                response = "The Stack is empty";
+            }
+            return response;
+        }
+    }
+}
---
+++ PWS_2/Lab2/Models/StackService.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab2.Models
+{
+    public static class StackService
+    {
+        private static int result = 0;
+        public static Stack<int> stack { get; private set; } = new Stack<int>();
+
+        public static string Get()
+        {
+            int _result = result;
+            if (stack.Count != 0)
+            {
+                _result += stack.Peek();
+            }
+            return JsonConvert.SerializeObject(new { RESULT = _result });
+        }
+
+        public static string Post(int _result)
+        {
+            result = _result;
+            return Get();
+        }
+
+        public static string Put(int add)
+        {
+            stack.Push(add);
+            return Get();
+        }
+
+        public static string Delete()
+        {
+            stack.Pop();
+            return Get();
+        }
+
+        public static int StackState()
+        {
+            return stack.Count();
+        }
+    }
+}
